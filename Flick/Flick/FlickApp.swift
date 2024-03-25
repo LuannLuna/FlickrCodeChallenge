dee32dd913c3c263ef6b2b3e8cd65a810e4b0560
luannluna <luann.marques@gmail.com>
2024-03-25 15:28:19 -0300

    created initial project

---
+++ Flick/Flick/FlickApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickApp.swift
+//  Flick
+//
+//  Created by Luann Luna on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
