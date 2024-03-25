2fe3023d346c58a2730f1470334c6548711f0467
luannluna <luann.marques@gmail.com>
2024-03-25 16:43:20 -0300

    created flickr model

---
+++ Flick/Flick/Model/Flickr.swift
@@ -0,0 +1,42 @@
+//
+//  Flickr.swift
+//  Flick
+//
+//  Created by Luann Luna on 25/03/24.
+//
+
+import Foundation
+
+// MARK: - Flickr
+struct FlickrResponse: Codable {
+    let title: String
+    let link: String
+    let description: String
+    let modified: Date
+    let generator: String
+    let items: [Thumb]
+}
+
+// MARK: - Item
+struct Thumb: Codable {
+    let title: String
+    let link: String
+    let media: Media
+    let dateTaken: Date
+    let description: String
+    let published: Date
+    let author, authorID, tags: String
+
+    enum CodingKeys: String, CodingKey {
+        case title, link, media
+        case dateTaken = "date_taken"
+        case description, published, author
+        case authorID = "author_id"
+        case tags
+    }
+}
+
+// MARK: - Media
+struct Media: Codable {
+    let m: String
+}
